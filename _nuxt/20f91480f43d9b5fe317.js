c414a515e0838e8f9c0fccf31a6f1f952926a8a3
233mac <service@233mac.com>
2020-02-23 23:27:52 +0800

    add _nuxt

---
+++ _nuxt/20f91480f43d9b5fe317.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3,5,7],{105:function(c,n,t){"use strict";var e={DESCRIPTION:"精品 Mac 应用软件分享"};n.a=e},24:function(c,n,t){"use strict";var e={FE_API_V1:"apiv1",SERVER_API_V1:"/apiv1",APP:"/app",ARTICLE:"/article",CDN_PREFIX:"https://cdn.jsdelivr.net/gh/233mac/233macCDN"};n.a=e}}]);
